fca4115ea8769a6319126378c1b8de25db514904
Courtland Erickson <courtland.erickson@gmail.com>
2026-01-08 14:40:30 -0600

    Lab Submit 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-function About({ image, about }) {
+function About({ image = "https://via.placeholder.com/215", about }) {
   return (
     <aside>
       <img src={image} alt="blog logo" />
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import blogData from "../data/blog";
-import Header from "./Header";
-import About from "./About";
-import ArticleList from "./ArticleList";
-
-console.log(blogData);
+import Header from "./Header.jsx";
+import About from "./About.jsx";
+import ArticleList from "./ArticleList.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header name={blogData.name} />
-      <About image={blogData.image} about={blogData.about} />
+      <About about={blogData.about} />
       <ArticleList posts={blogData.posts} />
     </div>
   );
